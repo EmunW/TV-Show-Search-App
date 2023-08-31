2fcd9192f6e9f65f9091b4427177d4cc96b3db3f
EmunW <edmundpunch5@gmail.com>
2023-08-30 23:58:02 -0400

    fetched tvmaze api and created table

--- main.js
+++ main.js
@@ -16,6 +16,55 @@
 // 		'X-RapidAPI-Host': 'transloc-api-1-2.p.rapidapi.com'
 // 	}
 // };
+const form = document.querySelector('#searchShows');
+form.addEventListener('submit', async function (e){
+    try{
+        // Need 'preventDefault' to prevent a form from doing its default function of sending its input to the url
+        e.preventDefault();
+        // 'elements' is a property of any form that stores all form controls as an 'HTMLFormControlsCollection'
+        // In this case we are looking for the 'q' value because we defined 'q' as the name of our input text in the html
+        const searchTerm = form.elements.q.value;
+        // Use a GET request to find shows that contain the words in 'searchTerm' and store them in result as an array
+        const result = await fetch(`https://api.tvmaze.com/search/shows?q=${searchTerm}`).then((data) => data.json());
+        // 'result[0]' is the first show in the array
+        console.log(result[0]);
+        // Remove the previous shows when doing a new search
+        if(document.querySelector('.allShows')){
+            document.querySelector('.allShows').remove();
+        }
+        generateTable(result);
+    }
+    catch(e){
+        console.log("error: " + e);
+    }
+})
+
+// Make a table of the shows we found???
+function generateTable(result){
+    const table = document.createElement('table');
+    table.classList.add('allShows')
+    const tableBody = document.createElement('tbody');
+
+    // Currently only adds images to the table
+    for(let i=0; i<result.length; i++){
+        // Create a row for the table
+        const row = document.createElement('tr');
+        // Create a cell for the row (This should be done in a nested for loop later if we want each column give different information about a show)
+        const cell = document.createElement('td');
+        // Create an image element to store in the cell
+        const img = document.createElement('IMG');
+
+        // Some shows seem to have no image so we will skip those
+        if(result[i].show.image !== null) img.src = result[i].show.image.medium;
+
+        tableBody.appendChild(row);
+        row.appendChild(cell)
+        cell.appendChild(img);
+    }
+    table.setAttribute('border', 2);
+    table.appendChild(tableBody);
+    document.body.appendChild(table);
+}
 
 // try {
 // 	const response = await fetch(url, options);
