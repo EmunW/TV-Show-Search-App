4738eb44a33ac2d33abf7a4769748091155e806d
EmunW <edmundpunch5@gmail.com>
2023-08-30 22:50:40 -0400

    Brainstorming

---
+++ main.js
@@ -0,0 +1,61 @@
+//Google Calander
+//change color of blocks even in the same category
+//add images maybe?
+
+//search bar to search TV show/ movie
+//display rating, desc
+
+//
+
+
+// const url = 'https://transloc-api-1-2.p.rapidapi.com/agencies.json?callback=call&geo_area=35.80176%2C-78.64347%7C35.78061%2C-78.68218&agencies=12';
+// const options = {
+// 	method: 'GET',
+// 	headers: {
+// 		'X-RapidAPI-Key': '2d411c4c3bmsh537300b59f9b0cbp1538c9jsnc35496b21e13',
+// 		'X-RapidAPI-Host': 'transloc-api-1-2.p.rapidapi.com'
+// 	}
+// };
+
+// try {
+// 	const response = await fetch(url, options);
+// 	const result = await response.text();
+// 	console.log(result);
+// } catch (error) {
+// 	console.error(error);
+// }
+
+
+
+// fetch('https://api.spotify.com/authorize', {
+//     client_id: '8947edd9ffb542d6bf1191ebda1917df',
+//     response_type: 'code',
+//     redirect_uri: 'http://localhost:3000',
+    
+// })
+
+
+
+//Rapid API ty for sp
+
+// async function fetchData() {
+
+// }
+
+
+// const url = 'https://calendarevents.p.rapidapi.com/calendar/a212edcb509733e73dca4ef55f447980c6b2b5db055aaa46bf3fda05f3c6e452@group.calendar.google.com/2';
+// const options = {
+// 	method: 'GET',
+// 	headers: {
+// 		'X-RapidAPI-Key': '2d411c4c3bmsh537300b59f9b0cbp1538c9jsnc35496b21e13',
+// 		'X-RapidAPI-Host': 'calendarevents.p.rapidapi.com'
+// 	}
+// };
+
+// try {
+// 	const response = await fetch(url, options);
+// 	const result = await response.text();
+// 	console.log(result);
+// } catch (error) {
+// 	console.error(error);
+// }
