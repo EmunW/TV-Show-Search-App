db189bf7dc340dd9c1322eeae9bd3d1cb3f05c73
EmunW <edmundpunch5@gmail.com>
2023-08-31 17:47:37 -0400

    Added more info

--- main.js
+++ main.js
@@ -27,7 +27,7 @@ form.addEventListener('submit', async function (e){
         // Use a GET request to find shows that contain the words in 'searchTerm' and store them in result as an array
         const result = await fetch(`https://api.tvmaze.com/search/shows?q=${searchTerm}`).then((data) => data.json());
         // 'result[0]' is the first show in the array
-        console.log(result[0]);
+        console.log(result);
         // Remove the previous shows when doing a new search
         if(document.querySelector('.allShows')){
             document.querySelector('.allShows').remove();
@@ -42,23 +42,64 @@ form.addEventListener('submit', async function (e){
 // Make a table of the shows we found???
 function generateTable(result){
     const table = document.createElement('table');
-    table.classList.add('allShows')
+    table.classList.add('allShows');
+    table.appendChild(generateHeaders());
+
     const tableBody = document.createElement('tbody');
     // Currently only adds images to the table
     for(let i=0; i<result.length; i++){
         // Create a row for the table
         const row = document.createElement('tr');
-        // Create a cell for the row (This should be done in a nested for loop later if we want each column give different information about a show)
-        const cell = document.createElement('td');
-        // Create an image element to store in the cell
-        const img = document.createElement('IMG');
+        for(let j=0; j<5; j++){
+            const show = result[i].show;
+            // Create a cell for the row (This should be done in a nested for loop later if we want each column give different information about a show)
+            const cell = document.createElement('td');
+            // Create an image element to store in the cell
 
-        // Some shows seem to have no image so we will skip those
-        if(result[i].show.image !== null) img.src = result[i].show.image.medium;
+            // Some shows seem to have no image so we will skip those
+            switch(j){
+                case 0:
+                    const img = document.createElement('IMG');
+                    if(show.image === null) {
+                        img.alt = 'NO IMAGE'
+                        cell.appendChild(img);
+                        break;
+                    }
+                    img.src = show.image.medium;
+                    cell.appendChild(img);
+                    break;
+                case 1:
+                    const title = document.createElement('text');
+                    title.innerHTML = `${show.name}`;
+                    cell.appendChild(title);
+                    break;
+                case 2:
+                    const summary = document.createElement('text');
+                    summary.innerHTML = show.summary;
+                    cell.appendChild(summary);
+                    break;
+                case 3:
+                    const rating = document.createElement('text');
+                    if(show.rating.average !== null) {
+                        rating.innerHTML = show.rating.average;
+                    }
+                    else rating.innerHTML = 'N/A'
+                    cell.appendChild(rating);
+                    break;
+                case 4:
+                    const status = document.createElement('text');
+                    if(show.status !== null) {
+                        status.innerHTML = show.status;
+                    }
+                    else status.innerHTML = 'N/A'
+                    cell.appendChild(status);
+                    console.log(status);
+                    break;
+            }
 
+            row.appendChild(cell)
+        }
         tableBody.appendChild(row);
-        row.appendChild(cell)
-        cell.appendChild(img);
     }
     table.setAttribute('border', 2);
     table.appendChild(tableBody);
@@ -68,8 +109,34 @@ function generateTable(result){
 function generateHeaders(){
     const tableHead = document.createElement('thead');
     const hRow = document.createElement('tr');
+    tableHead.appendChild(hRow);
+    //Image
+    const imageCell = document.createElement('td');
+    const image = document.createTextNode('')
+    imageCell.appendChild(image);
+    hRow.appendChild(imageCell);
+    //Title
     const titleCell = document.createElement('td');
     const title = document.createTextNode('Title')
+    titleCell.appendChild(title);
+    hRow.appendChild(titleCell);
+    //Summary
+    const summaryCell = document.createElement('td');
+    const summary = document.createTextNode('Summary')
+    summaryCell.appendChild(summary);
+    hRow.appendChild(summaryCell);
+    //Rating
+    const ratingCell = document.createElement('td');
+    const rating = document.createTextNode('Rating')
+    ratingCell.appendChild(rating);
+    hRow.appendChild(ratingCell);
+    //Status
+    const statusCell = document.createElement('td');
+    const status = document.createTextNode('Status')
+    statusCell.appendChild(status);
+    hRow.appendChild(statusCell);
+    return tableHead;
+    
 }
 
 // try {
